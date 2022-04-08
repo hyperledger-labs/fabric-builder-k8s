55e76b35fbb4a8857305cb16d179f18ed6baa34c
James Taylor <jamest@uk.ibm.com>
2022-04-08 12:47:35 +0100

    Initial release and run implementation
    The initial run implementation is just based on the k8s API in-cluster example to figure out any issues
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- internal/builder/release.go
+++ internal/builder/release.go
@@ -2,13 +2,31 @@
 
 package builder
 
-import "errors"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/otiai10/copy"
+)
 
 type Release struct {
 	BuildOutputDirectory   string
 	ReleaseOutputDirectory string
 }
 
-func (d *Release) Run() error {
-	return errors.New("not implemented yet")
+func (r *Release) Run() error {
+	imageSrcPath := filepath.Join(r.BuildOutputDirectory, "image.json")
+	imageDestPath := filepath.Join(r.ReleaseOutputDirectory, "image.json")
+	err := copy.Copy(imageSrcPath, imageDestPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error copying %s to %s: %s\n", imageSrcPath, imageDestPath, err)
+		return err
+	}
+
+	// TODO copy any META-INF
+	// metainfSrcPath := filepath.Join(r.BuildOutputDirectory, "META-INF")
+	// metainfDestPath := filepath.Join(r.ReleaseOutputDirectory, "META-INF")
+
+	return nil
 }
--- internal/builder/run.go
+++ internal/builder/run.go
@@ -2,7 +2,15 @@
 
 package builder
 
-import "errors"
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
 
 type Run struct {
 	BuildOutputDirectory string
@@ -10,5 +18,38 @@ type Run struct {
 }
 
 func (d *Run) Run() error {
-	return errors.New("not implemented yet")
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// get pods in all the namespaces by omitting namespace
+	// Or specify namespace to get pods in particular namespace
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	// Examples for error handling:
+	// - Use helper functions e.g. errors.IsNotFound()
+	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+	_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Printf("Found example-xxxxx pod in default namespace\n")
+	}
+
+	return nil
 }
