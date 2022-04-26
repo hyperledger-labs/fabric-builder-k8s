9de9a61aa3c1eb5ddef4cb018b5d29829ea29038
James Taylor <jamest@uk.ibm.com>
2022-04-26 17:04:09 +0100

    Update run
    - Use env vars for peer ID, and k8s namespace
    - Use local kubeconfig if specified via env var
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/hyperledgendary/fabric-builder-k8s/internal/builder"
+	"github.com/hyperledgendary/fabric-builder-k8s/internal/util"
 )
 
 func main() {
@@ -15,9 +16,26 @@ func main() {
 		os.Exit(1)
 	}
 
+	peerID, err := util.GetRequiredEnv("CORE_PEER_ID")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Expected CORE_PEER_ID environment variable")
+		os.Exit(1)
+	}
+
+	kubeconfigPath := util.GetOptionalEnv("KUBECONFIG_PATH", "")
+
+	kubeNamespace, err := util.GetRequiredEnv("KUBE_NAMESPACE")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Expected KUBE_NAMESPACE environment variable")
+		os.Exit(1)
+	}
+
 	run := &builder.Run{
 		BuildOutputDirectory: os.Args[1],
 		RunMetadataDirectory: os.Args[2],
+		PeerID:               peerID,
+		KubeconfigPath:       kubeconfigPath,
+		KubeNamespace:        kubeNamespace,
 	}
 
 	if err := run.Run(); err != nil {
