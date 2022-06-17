163f95a35477148db897dbee5fdba6dd71f47bc5
James Taylor <jamest@uk.ibm.com>
2022-06-17 14:22:08 +0100

    Handle CouchDB index definitions
    If CouchDB index definitions are required for the chaincode, release is
    responsible for placing the indexes into the statedb/couchdb/indexes
    directory under RELEASE_OUTPUT_DIR. The indexes must have a .json
    extension.
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

---
+++ cmd/release/release_suite_test.go
@@ -0,0 +1,26 @@
+package main_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+)
+
+var releaseCmdPath string
+
+func TestRelease(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Release Suite")
+}
+
+var _ = BeforeSuite(func() {
+	var err error
+	releaseCmdPath, err = gexec.Build("github.com/hyperledgendary/fabric-builder-k8s/cmd/release")
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	gexec.CleanupBuildArtifacts()
+})
